aadac16178bb1405a9836c7d3184783ce9a6fb71
jewelrana3 <juyelrana7752@gmail.com>
2025-12-02 10:15:42 +0600

    ai tools comment hide

--- demoData/data.ts
+++ demoData/data.ts
@@ -1,10 +1,10 @@
 import hotel from "../public/dashboard/hotel.png";
-import { CiCirclePlus, CiSettings } from "react-icons/ci";
+import { CiCirclePlus } from "react-icons/ci";
 import shift from "../public/dashboard/sidebar/shift.svg";
 import hire from "../public/dashboard/sidebar/hire-employee.svg";
 import two from "../public/dashboard/sidebar/appoinmenttwo.svg";
 import jobIcon from "../public/dashboard/sidebar/job-icon.svg";
-import aiIcon from "../public/dashboard/sidebar/ai-icon.svg";
+// import aiIcon from "../public/dashboard/sidebar/ai-icon.svg";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { MdOutlineVerifiedUser } from "react-icons/md";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
@@ -16,7 +16,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const data = [
   { title: "My Posted Jobs", icon: jobIcon },
   { title: "Post Job", icon: CiCirclePlus },
-  { title: "AI Tools", icon: aiIcon },
+  // { title: "AI Tools", icon: aiIcon },
   { title: "Appointments", icon: two },
   { title: "Subscription Plan", icon: MdPodcasts },
   { title: "Hire Employees", icon: hire },
