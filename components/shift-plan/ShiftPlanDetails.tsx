dfee401d7a57394255a8b8164da969564ff61d9f
jewelrana3 <juyelrana7752@gmail.com>
2026-01-27 09:45:51 +0600

    website is done

--- components/shift-plan/ShiftPlanDetails.tsx
+++ components/shift-plan/ShiftPlanDetails.tsx
@@ -19,8 +19,6 @@ export default function ShiftPlanDetails({ details }: any) {
   const { name, email, phone, address } = details?.worker;
   const [loading, setLoading] = useState(false);
 
-  console.log("details", details);
-
   const handleSendShift = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
