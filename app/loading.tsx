4e3f7d43c30decf8fbcefafcd2a6baefcd6f9294
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-03-05 11:09:22 +0600

    feat: Implement a global loading UI component using `loading.tsx`.

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 export default function Loading() {
   return (
     <div className="flex items-center justify-center text-xl h-screen">
-      Loading...
+      Loading....
     </div>
   );
 }
